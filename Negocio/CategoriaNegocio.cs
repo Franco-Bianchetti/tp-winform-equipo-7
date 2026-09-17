33962650cc0bff2e63d283b757ce9c066da5ee1a
tobiasorellana86-dev <tobiasorellana86@gmail.com>
2026-09-17 17:54:03 -0300

    Sumo metodo agregar y modificar a CategoriaNegocio.cs para el frmAltaCategoria

--- Negocio/CategoriaNegocio.cs
+++ Negocio/CategoriaNegocio.cs
@@ -44,5 +44,47 @@ namespace Negocio
                 datos.cerrarConexion();
             }
         }
+    
+
+public void agregar(Categoria nueva)
+{
+    AccesoDatos datos = new AccesoDatos();
+
+    try
+    {
+        datos.setearConsulta("INSERT INTO CATEGORIAS (Descripcion) VALUES (@descripcion)");
+        datos.setearParametro("@descripcion", nueva.Descripcion);
+        datos.ejecutarAccion();
+    }
+    catch (Exception ex)
+    {
+        throw ex;
+    }
+    finally
+    {
+        datos.cerrarConexion();
+    }
+}
+
+public void modificar(Categoria categoria)
+{
+    AccesoDatos datos = new AccesoDatos();
+
+    try
+    {
+        datos.setearConsulta("UPDATE CATEGORIAS SET Descripcion = @descripcion WHERE Id = @id");
+        datos.setearParametro("@descripcion", categoria.Descripcion);
+        datos.setearParametro("@id", categoria.Id);
+        datos.ejecutarAccion();
+    }
+    catch (Exception ex)
+    {
+        throw ex;
+    }
+    finally
+    {
+        datos.cerrarConexion();
+    }
+}
     }
 }
