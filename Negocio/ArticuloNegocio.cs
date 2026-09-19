79af01d566557b1a4f94c3461f2d610172635f5b
Franco <franco.bianchetti@alumnos.frgp.utn.edu.ar>
2026-09-18 21:11:15 -0300

    Modificacion ArticuloNegocio

--- Negocio/ArticuloNegocio.cs
+++ Negocio/ArticuloNegocio.cs
@@ -228,7 +228,7 @@ namespace Negocio
                             break;
                     }
                 }
-                else // Marca
+                else 
                 {
                     switch (criterio)
                     {
